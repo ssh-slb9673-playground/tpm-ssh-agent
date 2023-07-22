4a47eb549f12151dda812071128a9b2d5f64d52e
Shiho Midorikawa <shiho.elliptic@gmail.com>
2023-07-23 06:05:02 +0900

    Use i2cdev crate

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
-use mcp2221::AvailableDevice;
-use mcp2221::Config;
-use mcp2221::Error;
-use mcp2221::Handle;
+extern crate i2cdev;
 
-fn tpm_read_identifiers(device: &mut Handle) -> Result<(u16, u16, u8), Error> {
-    let mut vid_did_read_buf: [u8; 4] = [0; 4];
-    let vid_did_write_buf: [u8; 1] = [0x48];
-    device.write_read_address(0x2e, &vid_did_write_buf, &mut vid_did_read_buf)?;
-    let tpm_vendor_id = vid_did_read_buf[1] as u16 * 256 + vid_did_read_buf[0] as u16;
-    let tpm_device_id = vid_did_read_buf[3] as u16 * 256 + vid_did_read_buf[2] as u16;
+use i2cdev::core::*;
+use i2cdev::linux::LinuxI2CDevice;
+use std::convert::From;
+use std::fmt;
 
-    let mut rid_read_buf: [u8; 1] = [0; 1];
-    let rid_write_buf: [u8; 1] = [0x4c];
-    device.write_read_address(0x2e, &rid_write_buf, &mut rid_read_buf)?;
-    Ok((tpm_vendor_id, tpm_device_id, rid_read_buf[0]))
+#[derive(Debug)]
+pub enum Error {
+    LinuxI2CError(i2cdev::linux::LinuxI2CError),
+    Unknown,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Error::LinuxI2CError(e) => write!(f, "{}", e),
+            Error::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+pub type TPMResult<T> = Result<T, Error>;
+
+impl From<i2cdev::linux::LinuxI2CError> for Error {
+    fn from(err: i2cdev::linux::LinuxI2CError) -> Error {
+        Error::LinuxI2CError(err)
+    }
+}
+
+fn tpm_read_identifiers(device: &mut LinuxI2CDevice) -> Result<(u16, u16, u8), Error> {
+    let mut read_vid_and_did_buf = [0u8; 4];
+    let mut read_rid_buf = [0u8; 1];
+
+    device.write(&[0x48])?;
+    device.read(&mut read_vid_and_did_buf)?;
+    device.write(&[0x4c])?;
+    device.read(&mut read_rid_buf)?;
+
+    let (tpm_vendor_id, tpm_device_id) = {
+        (
+            read_vid_and_did_buf[1] as u16 * 256 + read_vid_and_did_buf[0] as u16,
+            read_vid_and_did_buf[3] as u16 * 256 + read_vid_and_did_buf[2] as u16,
+        )
+    };
+
+    let tpm_revision_id = read_rid_buf[0];
+
+    Ok((tpm_vendor_id, tpm_device_id, tpm_revision_id))
 }
 
 fn main() -> Result<(), Error> {
-    let cfg: Config = Config::default();
-    let mut device = AvailableDevice::list().unwrap()[0].open(&cfg)?;
+    let mut dev = LinuxI2CDevice::new("/dev/i2c-9", 0x2e)?;
 
-    let (tpm_vendor_id, tpm_device_id, tpm_revision_id) = tpm_read_identifiers(&mut device)?;
+    let (tpm_vendor_id, tpm_device_id, tpm_revision_id) = tpm_read_identifiers(&mut dev)?;
     assert_eq!(tpm_vendor_id, 0x15d1);
     assert_eq!(tpm_device_id, 0x001c);
     assert_eq!(tpm_revision_id, 0x16);
@@ -28,13 +61,15 @@ fn main() -> Result<(), Error> {
     println!("[+] Device ID: {:04x}", tpm_device_id);
     println!("[+] Revision ID: {:01x}", tpm_revision_id);
 
+    /*
     let mut cap_read_buf: [u8; 4] = [0; 4];
     let cap_write_buf: [u8; 1] = [0x30];
-    device.write_read_address(0x2e, &cap_write_buf, &mut cap_read_buf)?;
+    dev.write_read_address(0x2e, &cap_write_buf, &mut cap_read_buf)?;
     println!(
         "[+] capabilities = {:02x} {:02x} {:02x} {:02x}",
         cap_read_buf[3], cap_read_buf[2], cap_read_buf[1], cap_read_buf[0]
     );
+    */
 
     Ok(())
 }
